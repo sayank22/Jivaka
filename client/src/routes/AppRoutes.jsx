334d7f17f378ba727499f7178c68121ea401cfcb
sayank22 <sayank10023@gmail.com>
2025-06-04 19:31:34 +0530

    Added Clerk sign-in and sign-up routes

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { SignIn, SignUp } from '@clerk/clerk-react';
 
 import HomePage from '../pages/HomePage';
 import About from '../pages/About';
@@ -19,6 +20,16 @@ const AppRoutes = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/login/:role" element={<LoginPage />} />
+
+        {/* Clerk Auth Routes */}
+        <Route
+          path="/sign-in"
+          element={<SignIn routing="path" path="/sign-in" redirectUrl="/" />}
+        />
+        <Route
+          path="/sign-up"
+          element={<SignUp routing="path" path="/sign-up" redirectUrl="/" />}
+        />
 
         {/* Protected Routes */}
         <Route
